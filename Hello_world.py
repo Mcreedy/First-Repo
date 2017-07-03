eb1ba9d17efdc7e89a8ff7fc905c5d8bfe1a4cec
Mack Reed <mack@appliedgenerics.com>
2017-07-03 13:12:33 +0100

    A cleaned up single commit

--- Hello_world.py
+++ Hello_world.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-global name
+global name, birthday
 
 name = input("Whats your name?")
+birthday = input("Whats your birthday?")
 
 print ("Hello",name)
+print ("Have a good bithday on the",birthday)
 
